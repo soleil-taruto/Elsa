e3001b60106a7090bf14f8ba8fdcbecd5d3bc0bc
soleil-taruto <soleil.taruto@gmail.com>
2021-02-04 08:50:02 +0900

    Backup 20210204084827

---
+++ e20201265_NovelAdv_Base2/Elsa20200001/Elsa20200001/Games/Surfaces/Surface_30a830d530a730af30c8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte.Games.Surfaces
+{
+	public class Surface_エフェクト : Surface
+	{
+		public Surface_エフェクト(string typeName, string instanceName)
+			: base(typeName, instanceName)
+		{
+			this.Z = 50000;
+		}
+
+		public override IEnumerable<bool> E_Draw()
+		{
+			for (; ; )
+			{
+				// none
+
+				yield return true;
+			}
+		}
+	}
+}
